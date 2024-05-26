8e041edac53083ae13b356e63dfc0970437fb351
Yasmin Ferreira <yasmincferreira8@gmail.com>
2024-05-26 00:46:18 -0400

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <section>
+        background image
+        <div className='grid grid-cols-1 gap-8 lg:grid-cols-2'>
+          <div>
+            <article>Front card</article>
+            <article>back card</article>
+          </div>
+          <div>form</div>
+        </div>
+      </section>
+    </>
   )
 }
 
